3a7ac1341140dacd29b9b6bef9101978cb119094
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-03-02 02:46:21 +0800

    Added edit and update routes for comments

--- routes/comments.js
+++ routes/comments.js
@@ -52,4 +52,33 @@ router.post("/campgrounds/:id/comments", isLoggedIn, (req, res) => {
     });
 });
 
+//EDIT Route
+router.get("/campgrounds/:id/comments/:comment_id/edit", (req, res) => {
+  const { id: campgroundId, comment_id: commentId } = req.params;
+
+  Comment.findById(commentId)
+    .then(comment => {
+      res.render("comments/edit", { campgroundId, comment });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("back");
+    });
+});
+
+//UPDATE Route
+router.put("/campgrounds/:id/comments/:comment_id", (req, res) => {
+  const { id: campgroundId, comment_id: commentId } = req.params;
+  const { comment: updatedComment } = req.body;
+
+  Comment.findByIdAndUpdate(commentId, updatedComment)
+    .then(() => {
+      res.redirect(`/campgrounds/${campgroundId}`);
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("back");
+    });
+});
+
 module.exports = router;
