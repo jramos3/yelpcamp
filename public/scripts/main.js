709219b52cb0c554e4b8472a08bdd6afc19bd88c
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-03-03 22:47:22 +0800

    Styled the login and register page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,8 @@
-$(document).ready(() => {
-  setTimeout(() => {
-    $(".alert").alert("close");
-  }, 5000);
-});
+// $(document).ready(() => {
+//   setTimeout(() => {
+//     $(".alert").alert("close");
+//   }, 5000);
+// });
 
 document.querySelectorAll(".deleteBtn").forEach(deleteBtn => {
   deleteBtn.addEventListener("click", event => {
