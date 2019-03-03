458923302bc6ec33c0e94c7cfd4d4b3fb475c91c
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-03-03 11:34:14 +0800

    Added confirmation before deleting campground or comment

---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+  setTimeout(() => {
+    $(".alert").alert("close");
+  }, 5000);
+});
+
+document.querySelectorAll(".deleteBtn").forEach(deleteBtn => {
+  deleteBtn.addEventListener("click", event => {
+    if (!confirm("Are you sure you want to delete this?")) {
+      event.preventDefault();
+    }
+  });
+});
