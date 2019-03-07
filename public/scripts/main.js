e5aed53077c56ca8f16ac16edb2fb8e7d8a3e87e
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-03-07 23:28:06 +0800

    Add format price on press of enter key

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,6 +4,15 @@
 //   }, 5000);
 // });
 
+const formatToCurrency = event => {
+  let val = event.target.value;
+
+  if (val) {
+    val = Math.round(val * 100) / 100;
+    event.target.value = val.toFixed(2);
+  }
+};
+
 document.querySelectorAll(".deleteBtn").forEach(deleteBtn => {
   deleteBtn.addEventListener("click", event => {
     if (!confirm("Are you sure you want to delete this?")) {
@@ -13,12 +22,13 @@ document.querySelectorAll(".deleteBtn").forEach(deleteBtn => {
 });
 
 document.querySelectorAll(".priceControl").forEach(priceCtrl => {
-  priceCtrl.addEventListener("blur", event => {
-    let val = event.target.value;
+  priceCtrl.addEventListener("blur", formatToCurrency);
+});
 
-    if (val) {
-      val = Math.round(val * 100) / 100;
-      event.target.value = val.toFixed(2);
+document.querySelectorAll(".priceControl").forEach(priceCtrl => {
+  priceCtrl.addEventListener("keypress", event => {
+    if (event.keyCode === 13) {
+      formatToCurrency(event);
     }
   });
 });
